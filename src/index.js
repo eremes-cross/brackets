5b93e5d3055c190f86b3d65024b66311635fba4d
eremes-cross <sergei.cross@yandex.by>
2022-07-29 19:41:58 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  let pairs = bracketsConfig.map(arr => arr.join('')); 
+  
+  while (pairs.length) {
+    let i = 0;
+
+    for (let item of pairs) {
+      if (str.indexOf(item) > -1) {
+        str = str.replace(item, '');
+      } else {i++}
+    }
+
+    if (i === pairs.length) pairs.length = 0;  
+  }
+
+  return (!str) ? true : false;
+
+} 
